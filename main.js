f9d8a0334f1ad8f3cbba97f19ce7c0d2a8ce2789
Mark Santolucito <mark.santolucito@yale.edu>
2023-09-20 16:27:56 -0400

    more

--- main.js
+++ main.js
@@ -1,49 +1,90 @@
 document.getElementById('choice1').addEventListener("click", getNextPassageAndChoices);
 document.getElementById('choice2').addEventListener("click", getNextPassageAndChoices);
 
-async function getNextPassageAndChoices() {
- 	const currentText = document.getElementById('adventureText').innerHTML;
-	let passagePrompt = [
-  	{role: "system", content: "You are writing a choose your own adventure book. Compose a passage of the story. The passage should end just before a critical choice. Do not specify choices. Write in the present tense."},
-    {role: "user", content: currentText + " " + this.innerHTML}, //need to include some summarization/selection of rpevious turns to give context to the story
+passages = []
+storySummary = ""
+const numPassagesToConsider = 3;
+
+function firstRound(currentText) {
+  return currentText == "Once upon a time..."
+}
+
+function goToMarket(currentText) {
+  return true
+}
+
+//this doesnt work yet
+async function updateSummary(nextPassage) {
+  passages.push(nextPassage);
+  let summaryPrompt = [
+    { role: "system", content: "You are writing a book and need to recall important points of the story so far. Summarize the provided passages into a list of key facts about the story so far." }, //maybe ask for different kinds of options here - as mediated by TSL?
+    { role: "user", content: passages.slice(-numPassagesToConsider).join(' ') },
   ];
-	openAIFetchAPI(passagePrompt, 1).then(newText => {
-  	let nextPassage = newText[0].message.content + "."
-  	document.getElementById('adventureText').innerHTML = nextPassage;
-    let choicesPrompt = [
-      {role: "system", content: "You are writing a choose your own adventure book. Given the passage, give the player's next options as concrete actions, such as going walking to the left. Refer the the reader as `You` and use the present active tense. Do not prefix options with numbers. Do not use the word `can`"}, //maybe ask for different kinds of options here - as medidated by TSL?
-      {role: "user", content: nextPassage},
-    ];
-    openAIFetchAPI(choicesPrompt, 2).then(userChoices => {
-      document.getElementById('choice1').innerHTML = userChoices[0].message.content + ".";
-      document.getElementById('choice2').innerHTML = userChoices[1].message.content + ".";
-    })
+  return openAIFetchAPI(summaryPrompt, 1, "");
+}
+
+async function getChoices(nextPassage) {
+  let choicesPrompt = [
+    { role: "system", content: "You are writing a choose your own adventure book. Given the passage, give a single next concrete action for the player, such as walking to the left. Refer the the reader as `You` and use the present active tense. Do not prefix options with numbers. Do not use the word `can`. Do not use the word `or`" }, //maybe ask for different kinds of options here - as mediated by TSL?
+    { role: "user", content: nextPassage },
+  ];
+  openAIFetchAPI(choicesPrompt, 2, ".").then(userChoices => {
+    document.getElementById('choice1').innerHTML = userChoices[0].message.content + ".";
+    document.getElementById('choice2').innerHTML = userChoices[1].message.content + ".";
+  })
+}
+
+async function getNextPassageAndChoices() {
+  const currentText = document.getElementById('adventureText').innerHTML.trim();
+  document.getElementById('adventureText').innerHTML = "<div id=\"loading-bar-spinner\" class=\"spinner\"><div class=\"spinner-icon\"></div></div>";
+  let passagePrompt = [
+    { role: "system", content: "You are writing a choose your own adventure book. Compose a one paragraph-long passage of the story. The paragraph should end just before a critical choice. Do not specify choices. Write in the present tense." },
+    { role: "assistant", content: storySummary + " " + currentText },
+    { role: "user", content: this.innerHTML.replace("You", "I") },
+  ];
+
+  if (firstRound(currentText)) {
+    passagePrompt[0].content = passagePrompt[0].content + " Compose the introductory passage of the story which describes the character and the setting."
+  }
+  else if (goToMarket(currentText)) {
+    passagePrompt[0].content = passagePrompt[0].content + " Compose a passage where the reader visits a market to buy supplies."
+  }
+  console.log(passagePrompt[0].content)
+
+  openAIFetchAPI(passagePrompt, 1, "\n").then(newText => {
+    let nextPassage = newText[0].message.content;
+    document.getElementById('adventureText').innerHTML = nextPassage;
+    updateSummary(nextPassage).then(summary => {
+      storySummary = summary;
+      getChoices(nextPassage)
+    });
+
   });
 }
 
-async function openAIFetchAPI(promptMessages, numChoices) {
-    console.log("Calling GPT3")
-    const url = "https://api.openai.com/v1/chat/completions";
-    const YOUR_TOKEN = "sk-6r..." //add your own openai api key
-    const bearer = 'Bearer ' + YOUR_TOKEN
-    const data = await fetch(url, {
-        method: 'POST',
-        headers: {
-            'Authorization': bearer,
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            "model": "gpt-4",
-            "messages": promptMessages,
-            "max_tokens": 160,
-            "temperature": 1,
-            "top_p": 1,
-            "n": numChoices,
-            "stream": false,
-            "stop": ["\n", "."]
-        })
-    }).then(response => {
-        return response.json()
-    });
-    return data['choices'];
+async function openAIFetchAPI(promptMessages, numChoices, stopChars) {
+  console.log("Calling GPT3")
+  const url = "https://api.openai.com/v1/chat/completions";
+  const YOUR_TOKEN = "" //add your own openai api key
+  const bearer = 'Bearer ' + YOUR_TOKEN
+  const data = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Authorization': bearer,
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      "model": "gpt-4",
+      "messages": promptMessages,
+      "max_tokens": 160,
+      "temperature": 1,
+      "top_p": 1,
+      "n": numChoices,
+      "stream": false,
+      "stop": stopChars
+    })
+  }).then(response => {
+    return response.json()
+  });
+  return data['choices'];
 }
